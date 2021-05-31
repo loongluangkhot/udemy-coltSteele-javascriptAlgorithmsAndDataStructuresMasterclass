ae35dc027d90391e301686f2b1d939da7425ce25
loongluangkhot <loongluangkhot@gmail.com>
2021-05-31 23:52:13 +0800

    enhance quick-sort

--- algorithms/sorting/quick-sort.js
+++ algorithms/sorting/quick-sort.js
@@ -1,24 +1,23 @@
 function pivot(arr, start = 0, end = arr.length) {
   let pivot = arr[start];
   let swapIndex = start;
-  for(let i = start + 1; i < end; i++) {
-    if(pivot > arr[i]) {
-      swapIndex++;
+  for (let i = start + 1; i < end; i++) {
+    if (pivot > arr[i]) {
       [arr[swapIndex], arr[i]] = [arr[i], arr[swapIndex]];
+      swapIndex++;
     }
   }
-  [arr[swapIndex], arr[start]] = [arr[start], arr[swapIndex]];
   return swapIndex;
 }
 
 function quickSort(arr, start = 0, end = arr.length) {
-  if(end - start > 1) {
-    let pivotIndex = pivot(arr, start, end)
+  if (end - start > 1) {
+    let pivotIndex = pivot(arr, start, end);
     quickSort(arr, start, pivotIndex);
     quickSort(arr, pivotIndex + 1, end);
   }
   return arr;
 }
 
-arr = [5,2,1,8];
+arr = [5, 2, 1, 8, 4, 3, 7];
 console.log(quickSort(arr));
